130a05fda5ced54e63029e32549c44c38cc90bfe
Luca <luca.castellaro.lc@gmail.com>
2023-01-31 21:58:13 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::{env, fs, os::unix};
+use std::{env, fs::{self, remove_file}, os::unix, path::Path};
+
+use indicatif::ProgressBar;
 
 mod loaders_utils;
 
 fn main() {
     if change_dir().is_err()
-    {
-        return;
-    }
-    if backup_original().is_err()
     {
         return;
     }
@@ -36,25 +34,18 @@ fn change_dir() -> Result<(), ()> {
     }
 }
 
-fn backup_original() -> Result<(), ()> {
-    let spinner = loaders_utils::get_spinner("Copy original");
-    let result = fs::copy("video0", "video0.original");
-    match result {
-        Ok(_) => {
-            spinner.finish_with_message("Original copied");
-            return Ok(());
-        },
-        Err(err) => {
-            spinner.abandon();
-            println!("ERROR: {}", err.to_string());
-            return Err(());
-        }
-    }
-}
-
 fn create_link() -> Result<(), ()> {
     let spinner = loaders_utils::get_spinner("Create link");
-    let result = unix::fs::symlink("/dev/video2", "/dev/vide0");
+
+    let original = Path::new("/dev/video2");
+    let link = Path::new("/dev/video0");
+
+    let result = delete_file(link, &spinner);
+    if result.is_err() {
+        return Err(());
+    }
+
+    let result = unix::fs::symlink(original, link);
     match result {
         Ok(_) => {
             spinner.finish_with_message("Link created");
@@ -65,4 +56,25 @@ fn create_link() -> Result<(), ()> {
             println!("ERROR: {}", err.to_string());
             return Err(());
         }
-    }}
+    }
+}
+
+fn delete_file(link: &Path, spinner: &ProgressBar) -> Result<(), ()> {
+    if Path::exists(link) {
+        spinner.set_message(format!("Delete {:?}", link));
+        let result = fs::remove_file(link);
+        match result {
+            Ok(_) => {
+                spinner.set_message(format!("File {:?} deleted", link));
+                return Ok(());
+            },
+            Err(err) => {
+                spinner.abandon();
+                println!("ERROR: {}", err.to_string());
+                return Err(());
+            }
+        }
+    }
+
+    return Ok(());
+}
